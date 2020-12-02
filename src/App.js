f636bb6322ab4792dab31241a4b8661b8ceee988
Danijela2019 <arsic_danijela@yahoo.com>
2020-12-02 08:44:04 +0100

    Changed the color sheme and added authentication screen

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import {MainBoard} from './components/MainBoard';
 import Checkout from './components/Checkout';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Orders from './components/Orders';
+import Auth from './components/Auth'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 
 function App() {
@@ -15,6 +18,7 @@ function App() {
       <Route path="/" exact component={ MainBoard} />
       <Route path="/checkout" component={Checkout} />
       <Route path="/orders" component={Orders} />
+      <Route path="/auth" component={Auth} />
       </Layout>
       </BrowserRouter>
     </div>
